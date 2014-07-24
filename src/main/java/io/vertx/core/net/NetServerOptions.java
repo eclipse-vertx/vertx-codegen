c18b4f8c6423c49c0048b2415421b7afcf055a28
Julien Viet <julien@julienviet.com>
2014-07-24 15:44:04 +0200

    Provide json constructor for test NetServerOptions stub

--- src/main/java/io/vertx/core/net/NetServerOptions.java
+++ src/main/java/io/vertx/core/net/NetServerOptions.java
@@ -1,10 +1,14 @@
 package io.vertx.core.net;
 
 import io.vertx.codegen.annotations.Options;
+import io.vertx.core.json.JsonObject;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 @Options
 public class NetServerOptions {
+
+  public NetServerOptions(JsonObject json) {
+  }
 }
