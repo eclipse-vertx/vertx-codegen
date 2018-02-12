7d94b5dff4a5039e7d1a9e3d022de9eae00a9a65
Paulo Lopes <paulo@mlopes.net>
2018-02-12 19:24:08 +0100

    Added basic test

--- src/test/java/io/vertx/test/codegen/EnumTest.java
+++ src/test/java/io/vertx/test/codegen/EnumTest.java
@@ -1,9 +1,9 @@
 package io.vertx.test.codegen;
 
-import io.vertx.codegen.EnumModel;
-import io.vertx.codegen.EnumValueInfo;
-import io.vertx.codegen.GenException;
-import io.vertx.codegen.Generator;
+import io.vertx.codegen.*;
+import io.vertx.codegen.type.EnumTypeInfo;
+import io.vertx.codegen.type.TypeInfo;
+import io.vertx.test.codegen.testenum.EnumAsParam;
 import io.vertx.test.codegen.testenum.InvalidEmptyEnum;
 import io.vertx.test.codegen.testenum.ValidEnum;
 import org.junit.Test;
@@ -41,5 +41,14 @@ public class EnumTest {
       fail();
     } catch (GenException ignore) {
     }
+  }
+
+  @Test
+  public void testEnumListingFromApi() throws Exception {
+    ClassModel model = new Generator().generateClass(EnumAsParam.class);
+    assertTrue(model.getReferencedEnumTypes().size() > 0);
+    TypeInfo typeInfo = (TypeInfo) model.getReferencedEnumTypes().toArray()[0];
+    assertTrue(typeInfo instanceof EnumTypeInfo);
+    assertEquals("ValidEnum", typeInfo.getSimpleName());
   }
 }
