5d2704d5805817c50934d72a7a41090a05532f16
Gunnar Morling <gunnar.morling@googlemail.com>
2018-03-15 23:17:31 +0100

    #161 Preparing handling of TypeInternal via MR JAR;
    * Moving MirrorTypeInternal and ReflectType into their own source files
    * Creating TypeInternalProviders class
     - returns all three providers under JDK 8
     - returns just reflection and mirror provider under JDK 9
    * Removing reflective lookup of TreeTypeInternal

---
+++ src/main/java9/io/vertx/codegen/type/TypeInternalProviders.java
@@ -0,0 +1,24 @@
+package io.vertx.codegen.type;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.vertx.codegen.type.TypeUse.TypeInternalProvider;
+
+/**
+ * Catalog of available {@link TypeInternalProvider}s.
+ *
+ * @author Gunnar Morling
+ */
+public class TypeInternalProviders {
+
+  /**
+   * Returns an immutable list of the type internal providers to be applied.
+   */
+  public static List<TypeInternalProvider> getProviders() {
+    return Arrays.asList(
+      ReflectTypeInternal.PROVIDER,
+      MirrorTypeInternal.PROVIDER
+    );
+  }
+}
