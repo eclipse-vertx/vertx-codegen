2cc22a821156fdb8b7a32d14dc1aa288bf31e999
ilayan19900722@gmail.com <liuyifei825601>
2018-03-19 16:42:09 +0800

    remove some old code

--- src/test/java/io/vertx/test/codegen/testdataobject/PropertyKindDataObject.java
+++ src/test/java/io/vertx/test/codegen/testdataobject/PropertyKindDataObject.java
@@ -8,13 +8,5 @@ import java.util.Set;
 
 @DataObject
 public interface PropertyKindDataObject {
-  int getInteger();
-  void setInteger(int i);
-  List<Integer> getIntegerList();
-  void setIntegerList(List<Integer> list);
-  Set<Integer> getIntegerSet();
-  void setIntegerSet(Set<Integer> list);
-  Map<String, String> getMap();
-  void setMap(Map<String, String> list);
-  void addMap(String key, String value);
+
 }
