d2d400b428f8ebce36558c328526a700627c1b1b
slinkydeveloper <francescoguard@gmail.com>
2019-02-09 10:52:55 +0100

    Added a test for DataObject model
    Signed-off-by: francesco <francescoguard@gmail.com>

---
+++ src/test/java/io/vertx/test/codegen/testdataobject/jsoncodec/MyPojo.java
@@ -0,0 +1,15 @@
+package io.vertx.test.codegen.testdataobject.jsoncodec;
+
+public class MyPojo {
+
+  Integer a;
+
+  public Integer getA() {
+    return a;
+  }
+
+  public MyPojo setA(Integer a) {
+    this.a = a;
+    return this;
+  }
+}
