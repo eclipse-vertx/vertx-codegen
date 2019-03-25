a9589fcdf680aca75afd5f4c95a741ac930ab2a4
slinkydeveloper <francescoguard@gmail.com>
2019-03-25 09:35:29 +0100

    Added Javadoc to JsonCodec interfaces
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/vertx/core/json/JsonDecoder.java
+++ src/main/java/io/vertx/core/json/JsonDecoder.java
@@ -1,5 +1,20 @@
 package io.vertx.core.json;
 
+import io.vertx.codegen.annotations.Nullable;
+
+/**
+ * Primitive for conversion JSON_TYPE -> TARGET_TYPE
+ *
+ * @param <TARGET_TYPE>
+ * @param <JSON_TYPE>
+ */
 public interface JsonDecoder<TARGET_TYPE, JSON_TYPE> {
-  TARGET_TYPE decode(JSON_TYPE value);
+  /**
+   * decode performs the conversion JSON_TYPE -> TARGET_TYPE.
+   * Note: This method must handle null values
+   *
+   * @param value
+   * @return
+   */
+  @Nullable TARGET_TYPE decode(@Nullable JSON_TYPE value);
 }
--- src/main/java/io/vertx/core/json/JsonEncoder.java
+++ src/main/java/io/vertx/core/json/JsonEncoder.java
@@ -1,5 +1,20 @@
 package io.vertx.core.json;
 
+import io.vertx.codegen.annotations.Nullable;
+
+/**
+ * Primitive for conversion TARGET_TYPE -> JSON_TYPE
+ *
+ * @param <TARGET_TYPE>
+ * @param <JSON_TYPE>
+ */
 public interface JsonEncoder<TARGET_TYPE, JSON_TYPE> {
-  JSON_TYPE encode(TARGET_TYPE value);
+  /**
+   * encode performs the conversion TARGET_TYPE -> JSON_TYPE.
+   * Note: This method must handle null values
+   *
+   * @param value
+   * @return
+   */
+  @Nullable JSON_TYPE encode(@Nullable TARGET_TYPE value);
 }
