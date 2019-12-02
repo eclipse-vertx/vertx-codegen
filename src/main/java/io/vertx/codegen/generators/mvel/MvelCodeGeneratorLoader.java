cf4d81067b65e769ffffd07dcbbf666fbce8ff06
Thomas Segismont <tsegismont@gmail.com>
2019-12-02 10:40:28 +0100

    MvelCodeGeneratorLoader: restore templateFilename and filename attribute alternatives
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/codegen/generators/mvel/MvelCodeGeneratorLoader.java
+++ src/main/java/io/vertx/codegen/generators/mvel/MvelCodeGeneratorLoader.java
@@ -92,10 +92,10 @@ public class MvelCodeGeneratorLoader implements GeneratorLoader {
       } else if ("incremental".equals(tokenName)) {
         parser.nextToken();
         gen.incremental = parser.getValueAsBoolean();
-      } else if ("filename".equals(tokenName)) {
+      } else if ("filename".equals(tokenName) || "fileName".equals(tokenName)) {
         parser.nextToken();
         gen.filename = parser.getValueAsString();
-      } else if ("templateFilename".equals(tokenName)) {
+      } else if ("templateFilename".equals(tokenName) || "templateFileName".equals(tokenName)) {
         parser.nextToken();
         gen.templateFilename = parser.getValueAsString();
       }
