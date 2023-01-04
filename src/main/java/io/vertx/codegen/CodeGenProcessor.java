81178ae1334936e6f625d34a64c4ccf71952ae2b
Thomas Segismont <tsegismont@gmail.com>
2023-01-04 10:13:47 +0100

    Modify defaultKaptGeneratedLocation only when necessary
    Only on Windows we need to modify the path separator
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/codegen/CodeGenProcessor.java
+++ src/main/java/io/vertx/codegen/CodeGenProcessor.java
@@ -223,7 +223,9 @@ public class CodeGenProcessor extends AbstractProcessor {
     if (exception != null) {
       String kaptGeneratedLocation = processingEnv.getOptions().get("kapt.kotlin.generated");
       String defaultKaptGeneratedLocation = "/build/generated/source/kaptKotlin/main";
-      defaultKaptGeneratedLocation = defaultKaptGeneratedLocation.replace('/', File.separatorChar);
+      if (System.getProperty("os.name").toLowerCase(Locale.US).contains("win")) {
+        defaultKaptGeneratedLocation = defaultKaptGeneratedLocation.replace('/', File.separatorChar);
+      }
 
       if (kaptGeneratedLocation != null && kaptGeneratedLocation.endsWith(defaultKaptGeneratedLocation)) {
         File projectDir = new File(kaptGeneratedLocation.substring(0, kaptGeneratedLocation.length() - defaultKaptGeneratedLocation.length()));
