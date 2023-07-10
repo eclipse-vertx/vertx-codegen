12855a3f185e4283d0af0b95081a52ad35022a9e
Jason Lee <lwlee2608@gmail.com>
2023-07-10 12:06:16 +0800

    initial commit for proto file gen

--- vertx-codegen-protobuf/src/main/java/io/vertx/codegen/protobuf/generator/ProtoFileGen.java
+++ vertx-codegen-protobuf/src/main/java/io/vertx/codegen/protobuf/generator/ProtoFileGen.java
@@ -2,9 +2,13 @@ package io.vertx.codegen.protobuf.generator;
 
 import io.vertx.codegen.DataObjectModel;
 import io.vertx.codegen.Generator;
+import io.vertx.codegen.PropertyInfo;
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.annotations.ModuleGen;
+import io.vertx.codegen.type.ClassKind;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Collection;
@@ -31,6 +35,36 @@ public class ProtoFileGen extends Generator<DataObjectModel> {
 
   @Override
   public String render(DataObjectModel model, int index, int size, Map<String, Object> session) {
-    return "Generated " + model.getFqn() + "\n";
+    StringWriter buffer = new StringWriter();
+    PrintWriter writer = new PrintWriter(buffer);
+
+    if (index == 0) {
+      writer.print("syntax = \"proto3\";");
+      writer.print("option java_multiple_files = true;\n");
+      writer.print("option java_package = \"io.vertx.protobuf.generated\";\n");
+      writer.print("\n");
+      writer.print("package io.vertx.protobuf.generated;\n");
+      writer.print("\n");
+    }
+
+    String messageName = model.getType().getSimpleName();
+    int fieldNumber = 1;
+
+    writer.print("message " + messageName + "{\n");
+    for (PropertyInfo prop : model.getPropertyMap().values()) {
+      ClassKind propKind = prop.getType().getKind();
+      ProtoProperty protoProperty = ProtoProperty.getProtoProperty(prop, fieldNumber);
+      // TODO Map and List and builtin type
+      if (propKind.basic) {
+        writer.print("  " + protoProperty.getProtoType().value + " " + prop.getName() + " = " + fieldNumber + ";\n");
+      } else {
+        writer.print("  " + protoProperty.getMessage() + " " + prop.getName() + " = " + fieldNumber + ";\n");
+      }
+      fieldNumber++;
+    }
+    writer.print("}\n");
+    writer.print("\n");
+
+    return buffer.toString();
   }
 }
