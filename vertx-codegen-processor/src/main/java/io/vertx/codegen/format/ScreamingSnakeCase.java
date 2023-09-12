4c279fdcf568f047338b607bc7bccc6e7a19d273
Edward Vaisman <10497078+eddyv@users.noreply.github.com>
2023-09-12 15:58:11 -0400

    update javadoc

--- vertx-codegen-processor/src/main/java/io/vertx/codegen/format/ScreamingSnakeCase.java
+++ vertx-codegen-processor/src/main/java/io/vertx/codegen/format/ScreamingSnakeCase.java
@@ -3,11 +3,7 @@ package io.vertx.codegen.format;
 import java.util.List;
 import java.util.regex.Pattern;
 
-/**
- * Snake case, for instance {@literal foo_bar}.
- *
- * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
- */
+/** Screaming Snake case, for instance {@literal FOO_BAR}. */
 public class ScreamingSnakeCase extends Case {
 
   /** A {@code SnakeCase} instance. */
