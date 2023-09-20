cb5d504377cbd444e8e7aec51a3aabd75eb90dfe
Julien Viet <julien@julienviet.com>
2023-09-20 11:21:39 +0200

    Add support of unbounded wildcard types to GenIgnore permitted types.

--- vertx-codegen-processor/src/tck/java/io/vertx/codegen/testmodel/AnyJavaTypeTCK.java
+++ vertx-codegen-processor/src/tck/java/io/vertx/codegen/testmodel/AnyJavaTypeTCK.java
@@ -17,20 +17,23 @@ public interface AnyJavaTypeTCK {
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithListOfJavaTypeParam(List<Socket> socketList);
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithSetOfJavaTypeParam(Set<Socket> socketSet);
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithMapOfJavaTypeParam(Map<String, Socket> socketMap);
+  @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithGenericWithWildcard(Iterable<?> iterable);
 
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Socket methodWithJavaTypeReturn();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) List<Socket> methodWithListOfJavaTypeReturn();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Set<Socket> methodWithSetOfJavaTypeReturn();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Map<String, Socket> methodWithMapOfJavaTypeReturn();
+  @GenIgnore(GenIgnore.PERMITTED_TYPE) Iterable<?> methodWithGenericWithWildcardReturn();
 
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithHandlerJavaTypeParam(Handler<Socket> socketHandler);
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithHandlerListOfJavaTypeParam(Handler<List<Socket>> socketListHandler);
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithHandlerSetOfJavaTypeParam(Handler<Set<Socket>> socketSetHandler);
   @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithHandlerMapOfJavaTypeParam(Handler<Map<String, Socket>> socketMapHandler);
+  @GenIgnore(GenIgnore.PERMITTED_TYPE) void methodWithHandlerGenericWithWildcardParam(Handler<Iterable<?>> iterableHandler);
 
-  @GenIgnore(GenIgnore.PERMITTED_TYPE)
-  Future<Socket> methodWithHandlerAsyncResultJavaTypeParam();
+  @GenIgnore(GenIgnore.PERMITTED_TYPE) Future<Socket> methodWithHandlerAsyncResultJavaTypeParam();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Future<List<Socket>> methodWithHandlerAsyncResultListOfJavaTypeParam();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Future<Set<Socket>> methodWithHandlerAsyncResultSetOfJavaTypeParam();
   @GenIgnore(GenIgnore.PERMITTED_TYPE) Future<Map<String, Socket>> methodWithHandlerAsyncResultMapOfJavaTypeParam();
+  @GenIgnore(GenIgnore.PERMITTED_TYPE) Future<Iterable<?>> methodWithHandlerAsyncResultGenericWithWildcardParam();
 }
